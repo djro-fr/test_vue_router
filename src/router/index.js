af47f324271534e4b1676005402aa6e3a09a288e
Sylvain Girault <syl@djro.fr>
2025-08-16 18:39:43 +0200

    alias et redirections

--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,18 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
+      path: '/a-propos',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+      alias: '/about'
     },
+    // {
+    //   path: '/about',
+    //   redirect:{name: 'about'}
+    // }
   ],
 })
 
